946333e6e615cfd78a4f6fc5d2283ff902ae4d62
RahulPatnaik <142738799+RahulPatnaik@users.noreply.github.com>
2023-10-07 15:16:29 +0530

    .

--- Frame.py
+++ Frame.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import *
 
-
 root = tk.Tk()
 frame = Frame(root)
 frame.pack()
@@ -10,14 +9,13 @@ bottomframe = Frame(root)
 bottomframe.pack(side = BOTTOM)
 
 redbutton = Button(frame, text = "Hi", fg = "red")
-redbutton.pack(side = BOTTOM)
+redbutton.pack(side = LEFT)
 
 def open_window():
-    new_window = tk.Toplevel()
+    new_window = tk.Tk()
     label = tk.Label(new_window, text = "This is a new window")
     label.pack()
     new_window.mainloop()
-
 
 button = tk.Button(root, text= "Open window", command = open_window)
 button.pack()
