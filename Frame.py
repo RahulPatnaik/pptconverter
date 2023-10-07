a390ab01490309de6679156af2a2f989861e44e4
RahulPatnaik <142738799+RahulPatnaik@users.noreply.github.com>
2023-10-07 14:48:55 +0530

    Testing tkinter

---
+++ Frame.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import *
+
+
+root = tk.Tk()
+frame = Frame(root)
+frame.pack()
+
+bottomframe = Frame(root)
+bottomframe.pack(side = BOTTOM)
+
+redbutton = Button(frame, text = "Hi", fg = "red")
+redbutton.pack(side = BOTTOM)
+
+def open_window():
+    new_window = tk.Toplevel()
+    label = tk.Label(new_window, text = "This is a new window")
+    label.pack()
+    new_window.mainloop()
+
+
+button = tk.Button(root, text= "Open window", command = open_window)
+button.pack()
+
+
+
+root.mainloop()
